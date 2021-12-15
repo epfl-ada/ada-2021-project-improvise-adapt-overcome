43e235e2e98f4d63ade2433f0dd574fcf3029d3b
Luca Bataillard <lucabad@bluewin.ch>
2021-12-15 17:42:23 +0100

    create cluster info panel

---
+++ website/assets/js/panel.js
@@ -0,0 +1,50 @@
+//import { highlightOn, highlightOff } from "/assets/js/clusters.js";
+
+const highlightOn = (x) => console.log("Selecting " + x);
+const highlightOff = (x) => console.log("Unselecting " + selected_button_id);
+const SELECTOR_ID = "panel-selector";
+const BTN_CLASS = "btn";
+const BTN_SELECTED_CLASS = `${BTN_CLASS} btn-selected`;
+
+let selected_button = null;
+let selected_button_id = null;
+
+export function initPanel(clusters) {
+  const panelSelector = document.getElementById(SELECTOR_ID);
+
+  for (let cluster of clusters) {
+    const button = makeButton(cluster);
+    panelSelector.appendChild(button);
+  }
+}
+
+function makeButton(cluster) {
+  const button = document.createElement("button");
+  button.setAttribute("class", BTN_CLASS);
+  button.innerHTML = cluster.name;
+
+  button.onclick = () => {
+    if (selected_button_id !== cluster.cluster_id) {
+      if (selected_button) {
+        selected_button.setAttribute("class", BTN_CLASS);
+        highlightOff(selected_button_id);
+      }
+
+      button.setAttribute("class", BTN_SELECTED_CLASS);
+
+      selected_button_id = cluster.cluster_id;
+      selected_button = button;
+
+      highlightOn(selected_button_id);
+    }
+  };
+
+  return button;
+}
+
+function makeCluster(id) {
+  return {
+    cluster_id: id,
+    name: `Health and science ${id}`,
+  };
+}
