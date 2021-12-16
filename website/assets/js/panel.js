8d8ff731b8d55386124f5887cec8722b7f111eb4
Luca Bataillard <lucabad@bluewin.ch>
2021-12-16 18:49:54 +0100

    add cluster table info

--- website/assets/js/panel.js
+++ website/assets/js/panel.js
@@ -23,7 +23,7 @@ export function initPanel(clusters) {
 function makeButton(cluster, n_clusters) {
   const button = document.createElement("button");
   const color = clusterToRGB(cluster.cluster_id, n_clusters);
-  button.setAttribute("class", BTN_CLASS);
+  button.className = BTN_CLASS;
   button.innerHTML = cluster.name;
 
   button.style = `background-color: ${color}`;
@@ -35,7 +35,7 @@ function makeButton(cluster, n_clusters) {
         highlightOff();
       }
 
-      button.setAttribute("class", BTN_SELECTED_CLASS);
+      button.className = BTN_SELECTED_CLASS;
 
       selected_button_id = cluster.cluster_id;
       selected_button = button;
@@ -56,8 +56,52 @@ function makeButton(cluster, n_clusters) {
 }
 
 function setClusterInfo(cluster, n_clusters) {
-  const panelInfo = document.getElementById("panel-cluster");
+  const panel = document.getElementById("panel-cluster");
+  panel.style.display = "block";
+
+  const panelTitle = document.getElementById("panel-cluster-title");
+  panelTitle.innerText = cluster.name;
+
+  const panelStats = document.getElementById("panel-cluster-stats");
+  const journalTable = makeJournalTable(cluster.journals);
+  const speakerTable = makeSpeakerTable(cluster.speakers);
+
+  panelStats.innerHTML = journalTable + speakerTable;
   console.log(cluster);
 }
 
-function removeClusterInfo() {}
+function removeClusterInfo() {
+  const panel = document.getElementById("panel-cluster");
+  panel.style.display = "none";
+}
+
+function makeJournalTable(journals) {
+  const header =
+    "<tr><th colspan=2>Top journals</th></tr>" +
+    "<tr><th>Rank</th><th>Name</th></tr>";
+  const itemToString = (journal, index) =>
+    `<td class="center">${parseInt(index) + 1}</td><td>${journal}</td>`;
+
+  return makeTable(journals, header, itemToString);
+}
+
+function makeSpeakerTable(speakers) {
+  const header =
+    `<tr><th colspan=3>Top Quotes</th></tr>` +
+    `<tr><th>%</th><th>Speakers</th><th>Description</th></tr>`;
+  const itemToString = ([name, title, pct]) =>
+    `<td class="center">${(100 * pct).toFixed(
+      2
+    )}<td>${name}</td><td class="table-overflow">${title}</td>`;
+
+  return makeTable(speakers, header, itemToString);
+}
+
+function makeTable(items, header, itemToString = (x) => `<td>${x}</td>`) {
+  let tableItems = `${header}`;
+  for (let itemIdx in items) {
+    tableItems += `<tr>${itemToString(items[itemIdx], itemIdx)}</tr>`;
+  }
+
+  return `<div class="panel-table"><table>${tableItems}</table></div>`;
+}
