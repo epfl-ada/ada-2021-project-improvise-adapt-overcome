0c4d9b9dcdbdd7187cc499cbaf747cd2105172c6
Julian <julian.blackwell@epfl.ch>
2021-12-17 11:09:37 +0100

    Added utility python module for distances between latitude-longitude points

---
+++ src/utility-notebooks/distances.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+PI = np.pi
+EARTH_R = 6371 # Earth radius in km
+
+def deg2rad(d):
+    'Convert degrees to radians'
+    return d * (PI / 180)
+
+def distance(a, b):
+    ''' 
+    Use the Haversine formula to calculate the distance between two coordinates a & b
+    a, b: (float, float) (latitude, longitude)
+    '''
+    a_lat, a_lon, b_lat, b_lon = a[0], a[1], b[0], b[1]
+    d_lat = deg2rad(b_lat - a_lat)
+    d_lon = deg2rad(b_lon - a_lon)
+
+    a = np.sin(d_lat / 2)**2 + np.cos(deg2rad(lat_a)) * np.cos(deg2rad(lat_b)) * np.sin(d_lon / 2) * np.sin(d_lon / 2)
+    c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1 - a))
+    distance = EARTH_R * c
+    return distance
+
+def get_closest(points):
+    'Get point closest to all other points'
+    sum_dist = {}
+    for point in points:
+        sum_dist[point] = np.mean([distance(point, other) for other in points])
+    return min(sum_dist, key=sum_dist.get)
